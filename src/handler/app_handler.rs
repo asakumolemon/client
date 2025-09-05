e978edf2b5b7eeca1aa8cddf0c1d89637c95770e
lichengqing <00103944@lesso.com>
2025-09-05 16:30:12 +0800

    feat(handler): 添加根据ID获取文章的功能

--- src/handler/app_handler.rs
+++ src/handler/app_handler.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 use crate::{model::app_state::AppState};
 use crate::util::request_util::*;
+use server::model::article::CommonArticleReq;
 use server::model::{common::*, user::*};
 use tokio::time::sleep;
 pub struct App{
@@ -75,6 +76,30 @@ impl App {
                 println!("升级成功");
             } else {
                 println!("升级失败: {}", resp.msg);
+            }
+        }
+    }
+
+    pub async fn get_article_by_id(&self) {
+        let req = CommonRequest {
+            token: Some(self.app_state.token.clone()),
+            data: CommonArticleReq {
+                article_id: Some(1),
+                article_title: None,
+                author_id: Some(1),
+                username: self.app_state.username.clone(),
+                content: None,
+                user_id: Some(1),
+            },
+        };
+        let resp = self.request_util.get_article_by_id(req).await;
+        if let Ok(resp) = resp {
+            if resp.code == 0 {
+                if let Some(data) = resp.data {
+                    println!("{:?}", data);
+                }
+            } else {
+                println!("获取文章失败: {}", resp.msg);
             }
         }
     }
