00ee29ee30d289096a3db1c766f629d7846dac4e
lichengqing <00103944@lesso.com>
2025-09-11 15:44:19 +0800

    fix(文件工具): 修复文件读写功能并自动创建目录
    确保在读写文件前目标目录存在，自动创建缺失的目录。修改write_file函数参数名使其更清晰，并统一使用read_to_string读取文件内容

--- src/util/file_util.rs
+++ src/util/file_util.rs
@@ -1,14 +1,24 @@
 use std::fs;
+use std::path::Path;
 
 pub fn read_file(file_name: &str) -> String {
     let current_dir = std::env::current_dir().expect("无法获取当前目录");
     let folder_name = current_dir.join(dotenvy::var("UPLOAD_FOLDER").unwrap());
+    if !Path::new(&folder_name).exists() {
+        fs::create_dir_all(&folder_name).expect("Error creating folder");
+    }
     let file_path = folder_name.join(file_name);
-    // fs::read_to_string(file_path).expect("Error reading file");
-    file_path.to_str().unwrap().to_string()
+    fs::read_to_string(file_path).expect("Error reading file")
 }
 
-pub fn write_file(file_path: &str, content: &str) {
+pub fn write_file(title: &str, content: &str) {
+    let current_dir = std::env::current_dir().expect("无法获取当前目录");
+    let folder_name = current_dir.join(dotenvy::var("UPLOAD_FOLDER").unwrap());
+    if !Path::new(&folder_name).exists() {
+        fs::create_dir_all(&folder_name).expect("Error creating folder");
+    }
+    let file_path = folder_name.join(title);
+
     fs::write(file_path, content).expect("Error writing file");
 }
 
